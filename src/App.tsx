26ccea90e007ad950af25b88a0b9930deef1f025
Daniel Di Venere <danieldivenere@gmail.com>
2023-08-16 21:00:27 -0700

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,29 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
 import { Data } from "./Data";
-import Map, { Marker, Popup } from "react-map-gl";
+import Map from "react-map-gl";
 import { StyledApp } from "./App.styled";
+import { useQuery } from "@tanstack/react-query";
+import MapItems from "./MapItems";
 
 function App() {
-  const [data, setData] = useState<Data[]>([]);
-  const [selected, setSelected] = useState("");
-
-  useEffect(() => {
-    populateData();
-  }, []);
-
-  const populateData = async () => {
-    const { data: res } = await axios<Data[]>({
-      url: "http://164.92.105.157:2000/get_properties",
-      method: "GET",
-      params: { api_key: "asd723erbkjhabsd8213" },
-    });
-
-    setData(res);
-  };
-
-  console.log();
+  const { data } = useQuery<any, unknown, Data[], any>({
+    queryKey: ["get_properties"],
+  });
 
   return (
     <StyledApp>
       <Map
-        mapboxAccessToken="pk.eyJ1IjoiZGFubnlkaTEyIiwiYSI6ImNsbGVnejM4NDBnbmIzZ25nZTRvaTlmajEifQ.fp0Kus3cRBjo3TCGd0GF-w"
+        mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
         mapLib={import("mapbox-gl")}
         initialViewState={{
           longitude: -156.64771868530957,
           latitude: 20.913646483159667,
           zoom: 11.5,
         }}
-        onClick={(e) => console.log(e)}
-        onDrag={(e) => console.log(e)}
+        // onClick={(e) => console.log(e)}
+        // onDrag={(e) => console.log(e)}
         mapStyle="mapbox://styles/mapbox/streets-v9"
       >
-        {data.map((item) => (
-          <>
-            <Marker
-              key={item.address + "m"}
-              latitude={item.latitude}
-              longitude={item.longitude}
-              onClick={() => {
-                console.log(item.address, "hi");
-                setSelected(item.address);
-              }}
-            />
-            {selected === item.address && (
-              <Popup
-                key={item.address + "p"}
-                latitude={item.latitude}
-                longitude={item.longitude}
-                offset={10}
-              >
-                <div style={{ width: "20px", height: "100px" }}>
-                  heloo there
-                </div>
-              </Popup>
-            )}
-          </>
-        ))}
+        {!!data && <MapItems properties={data} />}
       </Map>
     </StyledApp>
   );
